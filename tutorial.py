921331b1fb8427eb59ebe45840f00ea364938977
IgorDerik <34719600+IgorDerik@users.noreply.github.com>
2021-04-28 11:54:18 +0300

    Update tutorial.py

--- tutorial.py
+++ tutorial.py
@@ -1,84 +1,54 @@
+from builtins import range
 from datetime import timedelta
-from textwrap import dedent
 
-# The DAG object; we'll need this to instantiate a DAG
-from airflow import DAG
-
-# Operators; we need this to operate!
-from airflow.operators.bash import BashOperator
+from airflow.models import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.dummy_operator import DummyOperator
 from airflow.utils.dates import days_ago
-# These args will get passed on to each operator
-# You can override them on a per-task basis during operator initialization
-default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'email': ['airflow@example.com'],
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
-    # 'queue': 'bash_queue',
-    # 'pool': 'backfill',
-    # 'priority_weight': 10,
-    # 'end_date': datetime(2016, 1, 1),
-    # 'wait_for_downstream': False,
-    # 'dag': dag,
-    # 'sla': timedelta(hours=2),
-    # 'execution_timeout': timedelta(seconds=300),
-    # 'on_failure_callback': some_function,
-    # 'on_success_callback': some_other_function,
-    # 'on_retry_callback': another_function,
-    # 'sla_miss_callback': yet_another_function,
-    # 'trigger_rule': 'all_success'
+
+from airflow.models import DAG
+from airflow.operators.python_operator import PythonOperator
+
+from airflow.operators.http_operator import SimpleHttpOperator
+import json
+
+args = {
+    'owner': 'Airflow',
+    'start_date': days_ago(2),
 }
-with DAG(
-    'tutorial',
-    default_args=default_args,
-    description='A simple tutorial DAG',
-    schedule_interval=timedelta(days=1),
-    start_date=days_ago(2),
-    tags=['example'],
-) as dag:
 
-    # t1, t2 and t3 are examples of tasks created by instantiating operators
-    t1 = BashOperator(
-        task_id='print_date',
-        bash_command='date',
-    )
+dag = DAG(
+    dag_id='convert_csv2avro_spark_and_livy-4',
+    default_args=args,
+    schedule_interval=None,
+    tags=['tutorial']
+)
 
-    t2 = BashOperator(
-        task_id='sleep',
-        depends_on_past=False,
-        bash_command='sleep 5',
-        retries=3,
-    )
-    dag.doc_md = __doc__
+sparkConverter = SimpleHttpOperator(task_id="converter",
+		http_conn_id="sparkLivyEndpoint",
+		endpoint="/batches/", 
+		method="POST",
+		data=json.dumps({
+			  "file": "/tmp/hdfs_data/sparktask-1.0-SNAPSHOT-jar-with-dependencies.jar",
+			  "className": "com.kozachuk.learning.spark.SparkTransfer",
+			  "args": [
+				"spark://hdfs.host.1.com:40123"
+			  ],
+			  "conf": {
+				"master": "hdfs.host.1.com:40123",
+				"driver-memory": "6g",
+				"executor-memory": "6g",
+				"packages": "org.apache.spark:spark-avro_2.11:2.4.6"
+			  }
+			})
+		, headers={"Content-Type" : "application/json"}
+		, log_response=True
+		, xcom_push=True
+		, dag=dag
+	)
 
-    t1.doc_md = dedent(
-        """\
-    #### Task Documentation
-    You can document your task using the attributes `doc_md` (markdown),
-    `doc` (plain text), `doc_rst`, `doc_json`, `doc_yaml` which gets
-    rendered in the UI's Task Instance Details page.
+start_task = DummyOperator( task_id='start', dag=dag)
+end_task = DummyOperator( task_id='end', dag=dag)
 
-    ![img](http://montcs.bloomu.edu/~bobmon/Semesters/2012-01/491/import%20soul.png)
-    """
-    )
-    templated_command = dedent(
-        """
-    {% for i in range(5) %}
-        echo "{{ ds }}"
-        echo "{{ macros.ds_add(ds, 7)}}"
-        echo "{{ params.my_param }}"
-    {% endfor %}
-    """
-    )
-
-    t3 = BashOperator(
-        task_id='templated',
-        depends_on_past=False,
-        bash_command=templated_command,
-        params={'my_param': 'Parameter I passed in'},
-    )
-
-    t1 >> [t2, t3]
+start_task >> sparkConverter
+sparkConverter >> end_task
